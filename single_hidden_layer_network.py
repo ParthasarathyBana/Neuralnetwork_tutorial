fixed issues with gradients

single_hidden_layer_network.py
@@ -30,47 +30,43 @@ import random
 class LinearTransform(object):
     # In this function you instantiate the member variables
     def __init__(self, W, b):
-        self.w = W
-        self.b = b
+        self.weight_matrix = W
+        self.bias_matrix = b
+        self.weight_momentum_matrix = np.zeros((self.weight_matrix.shape))
+        self.bias_momentum_matrix = np.zeros((self.bias_matrix.shape))
 
     # This is a fucntion for calculating the feed forward linear transform
     def forward(self, x):
-        self.x = x
-        # print("Linear Transform (forward):")
-        # print("x:", x.shape)
-        # print("w:", self.w.shape)
-        # print("b:", self.b.shape)
-        z_output = np.dot(self.x, self.w) + self.b
+        self.input_matrix = x
+        z_output = np.matmul(self.input_matrix, self.weight_matrix) + self.bias_matrix
         return z_output
 
     # This is a function for calculating the backward propagation for linear transform
-    def backward(self, grad_output, learning_rate=1e-3, momentum=0.0, l2_penalty=0.0):
-        # print("Linear Transform (backward):")
+    def backward(self, grad_output, momentum = 0.9, learning_rate=1e-5):
         y = grad_output
-        # print("y_shape: ", y.shape)
-        # print("x_shape: ", self.x.shape)
-        # print("y_transpose_shape: ", np.transpose(y))
-        # print("x_transpose_shape: ", np.transpose(self.x))
-        grad_input = np.dot(y, np.transpose(self.w))
-        grad_weights = np.dot(np.transpose(self.x), y) #np.transpose(np.dot(np.transpose(y), self.x))
+        grad_input = np.matmul(y, np.transpose(self.weight_matrix))
+        grad_weights = np.matmul(np.transpose(self.input_matrix), y) #np.transpose(np.dot(np.transpose(y), self.input_matrix))  
         grad_biases = np.sum(y, axis = 0)
-        self.w = self.w - learning_rate * grad_weights
-        self.b = self.b - learning_rate * grad_biases
-        return grad_input
+
+        self.weight_momentum_matrix = momentum * self.weight_momentum_matrix - learning_rate * grad_weights
+        self.weight_matrix += self.weight_momentum_matrix
+        self.bias_momentum_matrix = momentum * self.bias_momentum_matrix - learning_rate * grad_biases
+        self.bias_matrix += self.bias_momentum_matrix
+
+        return grad_input, grad_weights, grad_biases
 
 # This is a class for a ReLU layer max(x,0)
 class ReLU(object):
     # This is a function to calculate the Relu activation for feed forward transform
     def forward(self, x):
         self.x = x
-        # print("Relu forward:")
-        # print("x:", x.shape)
         self.relu_linear = np.maximum(0, self.x)
         return self.relu_linear
 
     # This is a function to calculate the Relu activation for backward propagation transform
-    def backward(self, grad_output, learning_rate=0.0, momentum=0.0, l2_penalty=0.0):
-        self.relu_gradient = self.x * grad_output
+    def backward(self, grad_output):
+        self.relu_gradient = grad_output
+        self.relu_gradient[self.relu_linear < 0.0] = 0.0
         return self.relu_gradient
 
 # This is a class for a sigmoid layer followed by a cross entropy layer, the reason 
@@ -82,8 +78,8 @@ class SigmoidCrossEntropy(object):
         return self.sigmoid_output
 
     # This is a function to calculate the Sigmoid activation for backward propagation transform 
-    def backward(self, grad_output, learning_rate=0.0, momentum=0.0, l2_penalty=0.0):
-        self.sigmoid_grad = (self.sigmoid_output - grad_output)
+    def backward(self, y, t):
+        self.sigmoid_grad = y - t
         return self.sigmoid_grad
 
 # This is a class for the Multilayer perceptron
@@ -91,59 +87,47 @@ class MLP(object):
 
     def __init__(self, input_dims, hidden_units):
     # INSERT CODE for initializing the network
-        self.w1 = np.random.randn(input_dims, hidden_units) * 0.1
+        self.w1 = np.random.randn(input_dims, hidden_units) * 0.01
         self.b1 = np.zeros((1, hidden_units))
-        self.w2 = np.random.randn(hidden_units, 1) * 0.1
+        self.w2 = np.random.randn(hidden_units, 1) * 0.01
         self.b2 = np.zeros((1, 1))
-        self.w1_m = 0
-        self.b1_m = 0
-        self.w2_m = 0
-        self.b2_m = 0
 
         self.linear_transform1 = LinearTransform(self.w1, self.b1)
         self.relu = ReLU()
         self.linear_transform2 = LinearTransform(self.w2, self.b2)
         self.sigmoid_entropy = SigmoidCrossEntropy()    
 
-    def loss_function(self, pred_y, target_y, l2_penalty):
-        # print("z_foward:",type(pred_y), "target_y:", type(target_y))
-        loss = -target_y*np.log(pred_y+1e-10)
-        loss = np.mean(loss)
+    def loss_function(self, pred_y, target_y, l2_penalty=0.01):
+        loss = -1 *( target_y * np.log(pred_y + 1e-8) + (1 - target_y) * np.log(1 - pred_y + 1e-8))
+        loss += l2_penalty / 2 * (np.sum(np.square(self.w1)) + np.sum(np.square(self.w2)))
         return loss
-        # loss = - target_y * np.log(pred_y) - (1 - target_y) * np.log(1 - pred_y)
-        # loss += l2_penalty / 2 * (np.sum(np.square(self.w1)) + np.sum(np.square(self.w2)))
-        # return loss
 
-    def train(self, x_batch, y_batch, learning_rate = 1e-3, momentum = 0.5, l2_penalty=0):
-        z_forward = self.linear_transform1.forward(x_batch)
-        z_forward = self.relu.forward(z_forward)
-        z_forward = self.linear_transform2.forward(z_forward)
-        z_forward = self.sigmoid_entropy.forward(z_forward)
-        # print("z_forward type: ", type(z_forward))
-        loss = self.loss_function(z_forward, y_batch, l2_penalty)
+    def train(self, x_batch, y_batch, l2_penalty=0):
+        z1_in = self.linear_transform1.forward(x_batch)
+        z1_out = self.relu.forward(z1_in)
+        z2_in = self.linear_transform2.forward(z1_out)
+        z2_out = self.sigmoid_entropy.forward(z2_in)
+        loss = self.loss_function(z2_out, y_batch, l2_penalty)
 
-        z_backward = self.sigmoid_entropy.backward(y_batch)
-        z_backward = self.linear_transform2.backward(z_backward)
-        z_backward = self.relu.backward(z_backward)
-        z_backward = self.linear_transform1.backward(z_backward)
+        z2_backward = self.sigmoid_entropy.backward(z2_out, y_batch)
+        d_x2, d_w2, d_b2 = self.linear_transform2.backward(z2_backward)
+        z1_backward = self.relu.backward(d_x2)
+        d_x1, d_w1, d_b1 = self.linear_transform1.backward(z1_backward)
 
-        average_loss = np.mean(self.loss_function(z_forward, y_batch, l2_penalty))
-        average_accuracy = 100 * np.mean((np.round(z_forward) == y_batch))
-        print(z_forward[:10])
-        return average_accuracy, loss        
+        average_loss = np.mean(loss)
+        average_accuracy = 100 * np.mean((np.round(z2_out) == y_batch))
+        return average_accuracy, average_loss        
 
     def evaluate(self, x, y):
-        z_forward = self.linear_transform1.forward(x_batch)
-        z_forward = self.relu.forward(z_forward)
-        z_forward = self.linear_transform2.forward(z_forward)
-        z_forward = self.sigmoid_entropy.forward(z_forward)
-
-        pred_y = np.round(z_forward)
-        loss = np.mean(self.loss_function(pred_y, y))
-
-    # INSERT CODE for testing the network
-    # ADD other operations and data entries in MLP if needed
-        pass
+        z1_in = self.linear_transform1.forward(x)
+        z1_out = self.relu.forward(z1_in)
+        z2_in = self.linear_transform2.forward(z1_out)
+        z2_out = self.sigmoid_entropy.forward(z2_in)
+        pred_y = z2_out
+        validation_loss = np.mean(self.loss_function(pred_y, y, l2_penalty = 0.01))
+        average_val_loss = np.mean(validation_loss)
+        average_val_acc = 100*np.mean((np.round(z2_out) == y))
+        return average_val_loss, average_val_acc
 
 if __name__ == '__main__':
     if sys.version_info[0] < 3:
@@ -151,47 +135,36 @@ if __name__ == '__main__':
     else:
         data = pickle.load(open('cifar_2class_py2.p', 'rb'), encoding='bytes')
 
-    train_x = data[b'train_data'] / 255
-    train_y = data[b'train_labels'] / 255
-    test_x = data[b'test_data']
+    train_x = data[b'train_data'] / 255.0
+    train_y = data[b'train_labels']
+    test_x = data[b'test_data']/255.0
     test_y = data[b'test_labels']
-    # print(train_x.shape)
+
     num_examples, input_dims = train_x.shape
-    # print("num_examples:", num_examples)
-    # print("input_dims:", input_dims)
-    # print(train_y.shape)
-    # INSERT YOUR CODE HERE
-    # YOU CAN CHANGE num_epochs AND num_batches TO YOUR DESIRED VALUES
-    num_epochs = 10
+    num_epochs = 200
     batch_size = 32
     average_train_accuracy = []
     average_train_loss = []
+    average_val_acc = []
+    average_val_loss = []
 
-    mlp = MLP(input_dims, hidden_units = 10)
-
+    mlp = MLP(input_dims, hidden_units = 16)
+    num_batches = num_examples // batch_size
     for epoch in range(num_epochs):
-        # random shuffling of data
-        # train_x = train_x[np.random.shuffle(np.arange(num_examples))]
-        # train_y = train_y[np.random.shuffle(np.arange(num_examples))]
-        # print("after shuffling")
-        # print("x_train: ",train_x.shape)
-        num_batches = num_examples // batch_size
+        rand_idx = np.arange(num_examples)
+        # print(max(rand_idx))
+        np.random.shuffle(rand_idx)
+        train_x = train_x[rand_idx]
+        train_y = train_y[rand_idx]
+        train_loss = 0.0
+        train_acc = 0.0
         for batch in range(num_batches):
-            train_accuracy, train_loss = mlp.train(train_x, train_y)
-            
-            average_train_accuracy.append(np.mean(train_accuracy))
-            average_train_loss.append(np.mean(train_loss))
-
-
-            # INSERT YOUR CODE FOR EACH MINI_BATCH HERE
-            # MAKE SURE TO UPDATE total_loss
-            print('\r[Epoch {}, mb {}]    Avg.Loss = {:.3f}    Avg.Accuracy = {:.3f}'.format(epoch + 1,batch + 1,np.mean(average_train_loss), np.mean(average_train_accuracy)))
-            sys.stdout.flush()
-        # INSERT YOUR CODE AFTER ALL MINI_BATCHES HERE
-        # MAKE SURE TO COMPUTE train_loss, train_accuracy, test_loss, test_accuracy
-        print()
-        print('    Train Loss: {:.3f}    Train Acc.: {:.2f}%'.format(average_train_loss,100.0 * average_train_accuracy))
-        # print('    Test Loss:  {:.3f}    Test Acc.:  {:.2f}%'.format(
-        #     test_loss,
-        #     100. * test_accuracy,
-        # ))
+            x_batch = train_x[batch * batch_size: (batch + 1) * batch_size]
+            y_batch = train_y[batch * batch_size: (batch + 1) * batch_size]
+            accuracy, loss = mlp.train(x_batch, y_batch)
+            train_loss += loss
+            train_acc += accuracy
+        train_loss = train_loss/num_batches
+        train_acc = train_acc/num_batches  
+        validation_loss, validation_acc = mlp.evaluate(test_x, test_y)
+        print(' Epoch:{}     Train Loss: {:.3f}    Train Acc.: {:.2f}%    Test Loss:  {:.3f}    Test Acc.:  {:.2f}%'.format(epoch, train_loss, train_acc, validation_loss, validation_acc))